54f633a6b3e9d046e85f400aa96eb50603fe5fc9
misu009 <96001559+misu009@users.noreply.github.com>
2025-04-22 16:28:23 +0300

    Update render-start.sh

--- render-start.sh
+++ render-start.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# Run Laravel migrations and seeders
+# Ensure Laravel config is cached
+php artisan config:clear
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# Run migrations and seeders
 php artisan migrate --force
 php artisan db:seed --force
 
-# Start Apache in foreground
+# Start Apache in the foreground
 apache2-foreground
